1c5e967f0e9c46a4da545b4a788c5def5491ef4d
sakal <sakals@mail.ua>
2019-06-07 18:53:53 -0400

    init: Favicon.
    Signed-off-by: sakal <sakals@mail.ua>

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -111,7 +111,7 @@ module.exports = (env, argv = {}) => {
           progressPlugin,
           cleanProdFolder,
           htmlIndex,
-          //favicon,
+          favicon,
         ]
         : [
           progressPlugin,
